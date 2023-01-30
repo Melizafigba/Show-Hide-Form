eeabb38cc3b469e9a49ebd2e713abebec9b01158
unknown <Maite@MFigueroa.saf.local>
2023-01-30 17:16:13 -0300

    formularios sin validar solo maqueta con funcionalidad show y hide en js

---
+++ main.js
@@ -0,0 +1,16 @@
+function toggleDiv(value) {
+  if (value == "" & "2") {
+    // alert("please select an option");
+  }
+
+  const box1 = document.getElementById('box1');
+  box1.style.display = value == 1 ? 'block' : 'none';
+
+
+  if (value == "" & "1") {
+
+  }
+  const box2 = document.getElementById('box2');
+  box2.style.display = value == 2 ? 'block' : 'none';
+
+}
